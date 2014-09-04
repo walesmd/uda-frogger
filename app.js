3993689b56b125b28908388a7b290825747ba49d
Mike Wales <michael.wales@udacity.com>
2014-09-04 07:52:42 -0700

    Initial commit.

---
+++ app.js
@@ -0,0 +1,113 @@
+var Enemy = function() {
+    this.xRange = [-150, 600];
+    this.possibleY = [60, 140, 220];
+    this.speedRange = [150, 600];
+
+    this.sprite = 'images/enemy-bug.png';
+
+    this.reset();
+}
+
+Enemy.prototype.reset = function() {
+    var startPos = this.xRange[0];
+
+    this.x = startPos;
+    this.y = this.getRandomY();
+    this.speed = this.getRandomSpeed();
+}
+
+Enemy.prototype.update = function(dt) {
+    var maxPos = this.xRange[1];
+    this.x += this.speed * dt;
+
+    if (this.x > maxPos) {
+        this.reset();
+    }
+}
+
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Enemy.prototype.getRandomY = function() {
+    return this.possibleY[Math.floor(Math.random() * this.possibleY.length)];
+}
+
+Enemy.prototype.getRandomSpeed = function() {
+    var minSpeed = this.speedRange[0],
+        maxSpeed = this.speedRange[1];
+
+    return Math.floor(Math.random() * (maxSpeed - minSpeed)) + minSpeed;
+}
+
+
+var Player = function() {
+    this.xRange = [-2, 402];
+    this.yRange = [-20, 380];
+    this.sprite = 'images/char-boy.png';
+    this.reset();
+}
+
+Player.prototype.update = function() {
+    this.checkCollisions();
+}
+
+Player.prototype.checkCollisions = function() {
+    if (this.y == -20) {
+        // player is on water, reset
+        this.reset();
+    } else if (this.y >= 60 && this.y <= 220) {
+        var self = this;
+        // player is on road rows, check collisions
+        // loop through each bug
+        allEnemies.forEach(function(enemy) {
+            // is the bug on the same row as the player?
+            if (enemy.y == self.y) {
+                // is the bug on the player?
+                if (enemy.x >= player.x - 30 && enemy.x <= player.x + 30) {
+                    self.reset();
+                }
+            }
+        });
+    }
+}
+
+Player.prototype.reset = function() {
+    this.x = 200;
+    this.y = 380;
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(key) {
+    if (key === 'left') {
+        this.x -= (this.x - 101 < this.xRange[0]) ? 0 : 101;
+    } else if (key === 'right') {
+        this.x += (this.x + 101 > this.xRange[1]) ? 0 : 101;
+    } else if (key === 'up') {
+        this.y -= (this.y - 80 < this.yRange[0]) ? 0 : 80;
+    } else if (key === 'down') {
+        this.y += (this.y + 80 > this.yRange[1]) ? 0 : 80;
+    }
+}
+
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
+
+
+var ada = new Enemy();
+var bob = new Enemy();
+var carl = new Enemy();
+var allEnemies = [ada, bob, carl];
+
+var player = new Player();
